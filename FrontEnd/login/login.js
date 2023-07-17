395675b92cd9277c6eef4917b5c4a8e66f39eea0
AlexandreCode2 <alexandremonrat2@gmail.com>
2023-07-17 18:25:32 +0200

    maj login.js

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -1,12 +1,13 @@
-let user = {
-    email : "",
-    password : "",
-};
-
-fetch('http://localhost:5678/api/users/login')
-.then(response => response.json())
-.then(data => {
-    displayUser(data);
-    user = data;
-    console.log(data);
+fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        email: 'sophie.bluel@test.tld',
+        password: 'S0phie'
+    })
 })
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log('Error:', error));
